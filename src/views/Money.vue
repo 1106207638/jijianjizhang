6f2a1e0d64b65aaa7613476cbe068991b3bc206f
1106207638 <1106207638@qq.com>
2020-12-09 17:39:57 +0800

    导航栏现在显示到页面底部

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    money.vue
+  <div class="Nav-wrapper">
+    <div class="content">
+      money.vue
+    </div>
     <Nav />
   </div>
 </template>
@@ -11,4 +13,15 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.Nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  flex-grow: 1;
+  overflow: auto;
+}
+</style>
