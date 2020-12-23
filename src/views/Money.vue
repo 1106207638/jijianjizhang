b9b8a62ba09ea7449dd2fb3504accd60f00d2244
1106207638 <1106207638@qq.com>
2020-12-23 21:21:17 +0800

    修复 tag.vue 不能添加标签的bug

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -9,7 +9,7 @@
         @update:value="onUpdateNotes"
       />
     </div>
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
+    <Tags />
   </Layout>
 </template>
 
@@ -23,19 +23,23 @@ import { Component } from "vue-property-decorator";
 import store from "@/store/index2.ts";
 @Component({
   components: { Tags, FormItem, Types, NumberPad },
+  computed: {
+    count() {
+      return store.count;
+    },
+    recordList() {
+      return store.recordList;
+    },
+  },
 })
 export default class Money extends Vue {
-  tags = store.tagList;
-  recordList = store.recordList;
+  store = store;
   record: RecordItem = {
     tags: [],
     notes: "",
     type: "-",
     amount: 0,
   };
-  onUpdateTags(value: string[]) {
-    this.record.tags = value;
-  }
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
